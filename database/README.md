3989b7e5b4912847c4c0e2a8dbb0886bee08a923
billy clark <billy_clark@presciencelabs.org>
2024-03-06 21:21:24 -0500

    initial

---
+++ database/README.md
@@ -0,0 +1,71 @@
+# Database
+
+https://www.sqlite.org
+
+## Convert the target mdb's to a sqlite database
+
+1. currently using a manual process, i.e., TBTA's `*.mdb` -> Google Drive -> MDB Viewer app -> download sqlite file (`*.YYYY-MM-DD.mdb.sqlite`)
+
+> if an mdb is larger than 40M, the MDB Viewer app will not work unfortunately.  There is an option to buy MDB ACCB Viewer (for macs).
+
+## Migrate TBTA's database to TaBiThA format
+1. `bun migrate.js English.YYYY-MM-DD.mdb.sqlite Targets.YYYY-MM-DD.tabitha.sqlite` to create and load the database
+
+## Interacting with the database locally
+
+### GUI
+
+https://sqlitebrowser.org has been a good tool and it's free
+
+### Command line
+
+`sqlite3` is needed, thankfully it's already installed on Mac, otherwise:  https://www.sqlite.org/download.html
+
+#### Getting help
+
+1. `sqlite3`
+1. `sqlite> .help` *https://www.sqlite.org/cli.html#special_commands_to_sqlite3_dot_commands_*
+1. `^d` to exit shell
+
+or
+
+https://www.sqlite.org/cli.html#command_line_options
+`sqlite3 -help`
+
+or
+
+`sqlite3 Targets.YYYY-MM-DD.tabitha.sqlite .help`
+
+### Dump
+
+`sqlite3 Targets.YYYY-MM-DD.tabitha.sqlite .dump > Targets.YYYY-MM-DD.tabitha.sqlite.sql`
+
+## Hosting service
+
+https://developers.cloudflare.com/d1
+https://developers.cloudflare.com/workers/wrangler/commands/#d1
+
+https://developers.cloudflare.com/workers/wrangler
+
+`pnpx wrangler ...` will also work if you do not want to install wrangler
+
+### List current databases
+
+`wrangler d1 list`
+
+### Create database
+
+`wrangler d1 create Targets.YYYY-MM-DD`
+
+### Interacting with the database
+
+> `--local` only operates on the local copy, removing that option will interact with the deployed database
+
+`wrangler d1 execute Targets.YYYY-MM-DD --local --file=./Targets.YYYY-MM-DD.tabitha.sqlite.sql`
+
+`wrangler d1 execute Targets.YYYY-MM-DD --local --command="select count(*) from English"`
+
+### Deployment
+
+D1 requires a worker so deployments will occur during the app deployment by virtue of the D1 binding.
+
---
+++ database/migrate.js
@@ -0,0 +1,79 @@
+import {Database} from 'bun:sqlite'
+
+const tbta_db_name = Bun.argv[2]
+const targets_db_name = Bun.argv[3]
+const tabitha_table_name = tbta_db_name.split('.')[0] // TBTA db becomes a table in Targets db
+
+console.log(`Creating ${tabitha_table_name} table in ${targets_db_name}...`)
+const targets_db = new Database(targets_db_name)
+targets_db.query(`
+	CREATE TABLE IF NOT EXISTS ${tabitha_table_name} (
+		book 		TEXT,
+		chapter 	INTEGER,
+		verse 	INTEGER,
+		text 		TEXT
+	)
+`).run()
+console.log('done.')
+
+
+console.log(`Extracting table names from ${tbta_db_name}...`)
+const tbta_db = new Database(tbta_db_name)
+// https://bun.sh/docs/api/sqlite#reference
+const target_tablenames_tbta = tbta_db.query(`
+	SELECT *
+	FROM sqlite_master
+	WHERE type = 'table' AND name like 'Target_EB_%'
+`).all().map(({name}) => name)
+console.log('done.')
+
+
+console.log(`Transforming data from ${tbta_db_name}...`)
+const transformed_data = target_tablenames_tbta.map(table_name => tbta_db.query(`
+		SELECT Reference, Verse
+		FROM ${table_name}
+	`).all().map(transform) // array of books
+).flat() // flattens all 66 books into one array of all verses
+
+function transform ({Reference, Verse}) {
+	// Reference looks like this: "Daniel 3:9"
+	const [, book, chapter, verse] = /(.*) (\d+):(\d+)/.exec(Reference)
+
+	// Verse looks like this: "Those astrologers said to King Nebuchadnezzar, “May you, the king, live forever!~!~(NP those[Pre-Nominal] astrologer+s[Plural] ) (VP said ) (NP to (NP _ King ) Nebuchadnezzar ) [ ,_“ (VP may[CP-VP] ) (NP you (NP , the[Pre-Nominal] king , ) ) (VP live ) (AdvP forever ) ! ] . Those astrologers said to King Nebuchadnezzar, “May you, the king, live forever! Those astrologers said to the king, “We hope that you'll live forever.~!~(NP those[Pre-Nominal] astrologer+s[Plural] ) (VP said ) (NP to the[Pre-Nominal] king ) [ ,_“ (NP we ) (VP hope ) [ that[Complementizer] (NP you ) (VP will[Pre-Verbal] live ) (AdvP forever ) ] ] . "
+	// 	note: can also be empty
+	const [, text] = /(.*?)~!~/.exec(Verse) ?? [,'']
+
+	return {
+		book,
+		chapter: Number(chapter),
+		verse: Number(verse),
+		text,
+	}
+}
+console.log('done.')
+
+
+console.log(`Cleaning existing data in ${tabitha_table_name}...`)
+targets_db.query(`
+	DELETE FROM ${tabitha_table_name}
+`).run()
+console.log('done.')
+
+
+console.log(`Loading data into ${tabitha_table_name} table...`)
+transformed_data.map(async ({book, chapter, verse, text}) => {
+	targets_db.query(`
+		INSERT INTO ${tabitha_table_name} (book, chapter, verse, text)
+		VALUES (?, ?, ?, ?)
+	`).run(book, chapter, verse, text)
+
+	await Bun.write(Bun.stdout, '.')
+})
+console.log('done.')
+
+
+console.log(`Optimizing ${targets_db_name}...`)
+targets_db.query(`
+	VACUUM
+`).run()
+console.log('done.')
