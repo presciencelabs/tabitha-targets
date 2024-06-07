3108ca1737b02f30324d59efbde7dbca3c7049e3
billy clark <billy_clark@presciencelabs.org>
2024-06-07 06:57:48 -0400

    added form_names table

--- database/migrate.js
+++ database/migrate.js
@@ -2,6 +2,7 @@ import {Database} from 'bun:sqlite'
 import {migrate_text_table} from './migrate_text_table'
 import {migrate_lexicon_table} from './migrate_lexicon_table'
 import {migrate_lexical_forms} from './migrate_lexical_forms'
+import {migrate_form_names_table} from './migrate_form_names_table'
 
 // usage: `bun migrate.js English.YYYY-MM-DD.mdb.sqlite Targets.YYYY-MM-DD.tabitha.sqlite`
 const tbta_db_name 		= Bun.argv[2] 						// English.YYYY-MM-DD.mdb.sqlite
@@ -17,6 +18,7 @@ targets_db.exec('PRAGMA journal_mode = WAL')
 migrate_text_table(tbta_db, language, targets_db)
 migrate_lexicon_table(tbta_db, language, targets_db)
 migrate_lexical_forms(language, targets_db)
+migrate_form_names_table(language, targets_db)
 
 console.log(`Optimizing ${targets_db_name}...`)
 targets_db.query(`VACUUM`).run()
---
+++ database/migrate_form_names_table.js
@@ -0,0 +1,58 @@
+export function migrate_form_names_table(language, targets_db) {
+	create_tabitha_table(targets_db)
+
+	load_data(targets_db, language)
+}
+
+/** @param {import('bun:sqlite').Database} targets_db */
+function create_tabitha_table(targets_db) {
+	create()
+
+	empty_existing_table()
+
+	return targets_db
+
+	function create() {
+		console.log(`Creating Form_Names table in ${targets_db.filename}...`)
+
+		targets_db.query(`
+			CREATE TABLE IF NOT EXISTS Form_Names (
+				language			TEXT,
+				part_of_speech	TEXT,
+				name				TEXT,
+				position			INTEGER
+			)
+		`).run()
+
+		console.log('done.')
+	}
+
+	function empty_existing_table() {
+		console.log(`Cleaning existing data in Form_Names...`)
+
+		targets_db.query(`DELETE FROM Form_Names`).run()
+
+		console.log('done.')
+	}
+}
+
+/** @param {import('bun:sqlite').Database} targets_db */
+function load_data(targets_db, language) {
+	console.log(`Loading data into Form_Names table...`)
+
+	const data = [
+		[language, 'Noun', 'Plural', 1],
+		[language, 'Verb', 'Past', 1],
+		[language, 'Verb', 'Perfect', 2],
+		[language, 'Verb', 'Participle', 3]
+		[language, 'Verb', 'Third Singular Present', 4],
+		[language, 'Adjective', 'Comparative', 1],
+		[language, 'Adjective', 'Superlative', 2]
+		[language, 'Adverb', 'Comparative', 1]
+		[language, 'Adverb', 'Superlative', 2]
+	]
+
+	data.map(row => targets_db.query('INSERT INTO Form_Names VALUES (?, ?, ?, ?)').run(...row))
+
+	console.log('done.')
+}
