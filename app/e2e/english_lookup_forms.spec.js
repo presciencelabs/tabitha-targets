9420e567d49bcdf8cf5579b7a10c58cf5957cd58
billy clark <billy.clark@presciencelabs.org>
2024-06-24 11:21:41 -0400

    added support for wildcards
    had to refactor in order to distinguish whether a match was by the stem or by the forms.

--- app/e2e/english_lookup_forms.spec.js
+++ app/e2e/english_lookup_forms.spec.js
@@ -84,3 +84,61 @@ test('/English/lookup/forms?word=got', async ({ request }) => {
 	expect(returned_forms[2]).toEqual(expected_forms[2])
 	expect(returned_forms[3]).toEqual(expected_forms[3])
 })
+
+test('/English/lookup/forms?word=follow*', async ({ request }) => {
+	const test_word = 'follow*'
+	const expected_forms = [
+		{
+			stem: 'following',
+			part_of_speech: 'Adjective',
+			form: 'Stem',
+		},
+		{
+			stem: 'follower',
+			part_of_speech: 'Noun',
+			form: 'Stem',
+		},
+		{
+			stem: 'follow',
+			part_of_speech: 'Verb',
+			form: 'Stem',
+		},
+		{
+			stem: 'follower',
+			part_of_speech: 'Noun',
+			form: 'Plural',
+		},
+		{
+			stem: 'follow',
+			part_of_speech: 'Verb',
+			form: 'Past',
+		},
+		{
+			stem: 'follow',
+			part_of_speech: 'Verb',
+			form: 'Perfect',
+		},
+		{
+			stem: 'follow',
+			part_of_speech: 'Verb',
+			form: 'Participle',
+		},
+		{
+			stem: 'follow',
+			part_of_speech: 'Verb',
+			form: 'Third Singular Present',
+		},
+	]
+
+	const response = await request.get(`/English/lookup/forms?word=${test_word}`)
+	const returned_forms = await response.json()
+
+	expect(returned_forms).toHaveLength(expected_forms.length)
+	expect(returned_forms[0]).toEqual(expected_forms[0])
+	expect(returned_forms[1]).toEqual(expected_forms[1])
+	expect(returned_forms[2]).toEqual(expected_forms[2])
+	expect(returned_forms[3]).toEqual(expected_forms[3])
+	expect(returned_forms[4]).toEqual(expected_forms[4])
+	expect(returned_forms[5]).toEqual(expected_forms[5])
+	expect(returned_forms[6]).toEqual(expected_forms[6])
+})
--- app/src/routes/[language=valid_language]/lookup/forms/+server.js
+++ app/src/routes/[language=valid_language]/lookup/forms/+server.js
@@ -3,117 +3,138 @@ import { json } from '@sveltejs/kit'
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ locals: { db }, params: { language }, url: { searchParams } }) {
 	/** @type {string} */
-	const word = searchParams.get('word') ?? ''
+	const word = normalize_wildcards(searchParams.get('word') ?? '')
 
-	const sql = `
+	const stem_sql = `
 		SELECT *
 		FROM Lexicon
 		WHERE language = ?
-			AND (stem LIKE ? OR forms LIKE ?)
+			AND stem LIKE ?
+	`
+	const forms_sql = `
+		SELECT *
+		FROM Lexicon
+		WHERE language = ?
+			AND forms LIKE ?
 	`
 
 	/** @type {import('@cloudflare/workers-types').D1Result<DbRowLexicon>} */
-	const { results } = await db.prepare(sql).bind(language, word, `%|${word}|%`).all()
+	const { results: stem_matches } = await db.prepare(stem_sql).bind(language, `${word}`).all()
+	/** @type {import('@cloudflare/workers-types').D1Result<DbRowLexicon>} */
+	const { results: forms_matches } = await db.prepare(forms_sql).bind(language, `%|${word}|%`).all()
 
 	/** @type {LexicalForm[]} */
-	let forms = []
-	for (const { stem: base_stem, part_of_speech, constituents,  forms: encoded_forms } of results) {
-		const stem = derive_stem(base_stem, constituents)
-
-		if (!encoded_forms) {
-			forms.push({ stem, part_of_speech, form: 'Stem' })
-			continue
-		}
-
-		// when encoded_forms is not null, it's a pipe-separated string
-		// e.g., 'following' => '|followed|followed|following|follows|'
-		const matched_indices = encoded_forms
-			.split('|')											// ['', 'followed', 'followed', 'following', 'follows', '']
-			.filter(form => !!form)							// ['followed', 'followed', 'following', 'follows']
-			.map((form, i) => form === word ? i : -1) // [-1, -1, 2, -1]
-			.filter(i => i > -1)								// [2]
-
-		for (const i of matched_indices) {
-			const form = await get_form_name(db, language, part_of_speech, i + 1)
-			forms.push({ stem, part_of_speech, form })
-		}
-	}
+	const forms = await transform({ stem_matches, forms_matches })
 
 	return json(forms)
-}
 
-/**
- *
- * @param {string} base_stem
- * @param {string} constituents
- * @returns {string}
- */
-function derive_stem(base_stem, constituents) {
-	if (!constituents) {
-		return base_stem
+	/**
+	 * @param {{
+	 * 	stem_matches: DbRowLexicon[],
+	 * 	forms_matches: DbRowLexicon[]
+	 * }} db_matches
+	 */
+	async function transform({ stem_matches, forms_matches }) {
+		/** @type {LexicalForm[]} */
+		let forms = []
+
+		for (const { stem: base_stem, part_of_speech, constituents } of stem_matches) {
+			const stem = derive_stem({ base_stem, constituents })
+
+			forms.push({ stem, part_of_speech, form: 'Stem' })
+		}
+
+		for (const { stem: base_stem, part_of_speech, constituents, forms: encoded_forms } of forms_matches) {
+			const stem = derive_stem({ base_stem, constituents })
+
+			// encoded_forms is a pipe-separated string
+			// e.g., 'following' => '|followed|followed|following|follows|'
+			const matched_indices = trim_pipes(encoded_forms) 	// 'followed|followed|following|follows'
+				.split('|') 												// ['followed', 'followed', 'following', 'follows']
+				.map((form, i) => is_match(form) ? i : -1) 		// [-1, -1, 2, -1]
+				.filter(i => i > -1) 									// [2]
+
+			for (const i of matched_indices) {
+				const position = i + 1
+				const name = await get_form_name({ db, language, part_of_speech, position })
+				forms.push({ stem, part_of_speech, form: name })
+			}
+		}
+
+		return forms
+
+		/**
+		 * @param {{
+		* 		base_stem: string,
+		* 		constituents: string
+		*	}} input
+		*/
+		function derive_stem({ base_stem, constituents }) {
+			if (!constituents) {
+				return base_stem
+			}
+
+			// this string consists of the part we want and some further info in brackets that is not useful in the context of the lexicon
+			// e.g.:
+			// 	off[Adposition in VP]
+			// 	threshing[First Word of Compound Noun]
+			// 	over[Verbal Adposition moved to Direct Object]
+			// 	of Ono[Post-Nominal Modifier]
+			const constituent = constituents.split('[')[0] ?? ''
+
+			return `${base_stem} ${constituent}`
+		}
+
+		/** @param {string} encoded_forms  */
+		function trim_pipes(encoded_forms) {
+			const PIPE_IN_FRONT_OR_REAR = /^\||\|$/
+
+			return encoded_forms.replace(PIPE_IN_FRONT_OR_REAR, '')
+		}
+
+		/** @param {string} form */
+		function is_match(form) {
+			if (form === word) {
+				return true
+			}
+
+			if (!word.includes('%')) {
+				return false
+			}
+
+			const constructed_re = new RegExp(word.replaceAll('%', '.*'))
+
+			return constructed_re.test(form)
+		}
+
+		/**
+		 * @param {{
+		 * 	db: import('@cloudflare/workers-types').D1Database,
+		 * 	language: string,
+		 * 	part_of_speech: string,
+		 * 	position: number
+		 * }} input
+		 * @returns {Promise<string>}
+		 */
+		async function get_form_name({ db, language, part_of_speech, position }) {
+			const sql = `
+				SELECT *
+				FROM Form_Names
+				WHERE language = ?
+					AND part_of_speech = ?
+					AND position = ?
+			`
+
+			/** @type {import('@cloudflare/workers-types').D1Result<DbRowFormNames>} */
+			return await db.prepare(sql).bind(language, part_of_speech, position).first('name') ?? ''
+		}
 	}
 
-	// this string consists of the part we want and some further info in brackets that is not useful in the context of the lexicon
-	// e.g.:
-	// 	off[Adposition in VP]
-	// 	threshing[First Word of Compound Noun]
-	// 	over[Verbal Adposition moved to Direct Object]
-	// 	of Ono[Post-Nominal Modifier]
-	const constituent = constituents.split('[')[0] ?? ''
-
-	return `${base_stem} ${constituent}`
+	/**
+	 * @param {string} possible_wildard – a string that may contain wildcards, e.g., '*' or '#' or '%'
+	 * @returns {string} SQL-ready string, i.e., `%` for wildcards
+	 */
+	function normalize_wildcards(possible_wildard) {
+		return possible_wildard.replace(/[*#]/g, '%')
+	}
 }
-
-/**
- * @param {import('@cloudflare/workers-types').D1Database} db
- * @param {string} language
- * @param {string} part_of_speech
- * @param {number} position
- *
- * @returns {Promise<string>}
- */
-async function get_form_name(db, language, part_of_speech, position) {
-	const sql = `
-		SELECT *
-		FROM Form_Names
-		WHERE language = ?
-			AND part_of_speech = ?
-			AND position = ?
-	`
-
-	/** @type {import('@cloudflare/workers-types').D1Result<DbRowFormNames>} */
-	return await db.prepare(sql).bind(language, part_of_speech, position).first('name') ?? ''
-}
-
-// async function by_filter(filter) {
-// 	const normalized_q = normalize_wildcards(filter.q)
-
-// 	const prepared_stmts = {
-// 		stems: db.prepare(`
-// 			SELECT *
-// 			FROM Concepts
-// 			WHERE stem LIKE ?`).bind(normalized_q),
-// 		glosses: db.prepare(`
-// 			SELECT *
-// 			FROM Concepts
-// 			WHERE gloss LIKE ?`).bind(`%${normalized_q}%`),
-// 		all: db.prepare(`
-// 			SELECT *
-// 			FROM Concepts
-// 			WHERE stem LIKE ?
-// 				OR gloss LIKE ?`).bind(normalized_q, `%${normalized_q}%`),
-// 	}
-
-// 	/** @type {import('@cloudflare/workers-types').D1Result<DbRowConcept>} */
-// 	const { results } = await prepared_stmts[filter.scope].all()
-
-// 	return normalize(results)
-// }
-
-// /**
-//  * @param {string} possible_wildard – a string that may contain wildcards, e.g., '*' or '#' or '%'
-//  * @returns {string} SQL-ready string, i.e., `%` for wildcards
-//  */
-// function normalize_wildcards(possible_wildard) {
-// 	return possible_wildard.replace(/[*#]/g, '%')
-// }
