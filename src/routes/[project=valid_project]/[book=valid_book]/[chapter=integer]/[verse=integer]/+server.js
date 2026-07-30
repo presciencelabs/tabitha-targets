e3b4dd432bfedad654a61c6957c1d374342ad743
craigp_atw <craig_palmer@alltheword.org>
2026-07-29 21:14:11 -0700

    New db and use of ideal_text

--- src/routes/[project=valid_project]/[book=valid_book]/[chapter=integer]/[verse=integer]/+server.js
+++ src/routes/[project=valid_project]/[book=valid_book]/[chapter=integer]/[verse=integer]/+server.js
@@ -14,5 +14,23 @@ export async function GET({ locals: { db }, params: { project, book, chapter, ve
 	/** @type {import('@cloudflare/workers-types').D1Result<TextResult>} https://developers.cloudflare.com/d1/platform/client-api/#return-object */
 	const { results } = await db.prepare(sql).bind(project, book, chapter, verse).all()
 
+	const ideal_sql = `
+		SELECT DISTINCT ideal_text AS text, audience
+		FROM Ideal_Text
+		WHERE project = ?
+			AND book = ?
+			AND chapter = ?
+			AND verse = ?
+	`
+	/** @type {import('@cloudflare/workers-types').D1Result<TextResult>} https://developers.cloudflare.com/d1/platform/client-api/#return-object */
+	const { results: ideal_results } = await db.prepare(ideal_sql).bind(project, book, chapter, verse).all()
+
+	for (const result of results) {
+		const ideal_result = ideal_results.find(ir => ir.audience === result.audience)
+		if (ideal_result) {
+			result.ideal = ideal_result.text
+		}
+	}
+
 	return json(results)
 }
